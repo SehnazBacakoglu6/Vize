e6dbc989bdc031b504798dea1a17ebc66bb28fbb
SehnazBacakoglu6 <bacakoglusehnaz6@gmail.com>
2023-11-14 19:21:32 +0300

    1.2 testleri tamamlandı

--- 200401076/Labex1_Q1_200401076_sehnaz_bacakoglu.R
+++ 200401076/Labex1_Q1_200401076_sehnaz_bacakoglu.R
@@ -1,2 +1,18 @@
+#1.1
 url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
 download.file(url, destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv", mode = "wb")
+
+#1.2
+maps <- read.csv("MapsThatChangedOurWorld_StoryMap_Data.csv",sep=";", header = TRUE)
+if(dim(maps)[1]==10 && dim(maps)[2]==9){ #dim=boyutu verir
+  print("tamam")
+}else{
+  print("hatalı")
+}
+
+
+
+
+
+
+
--- 200401076/Labex1_Q1_tests_20401076_sehnaz_bacakoglu.R
+++ 200401076/Labex1_Q1_tests_20401076_sehnaz_bacakoglu.R
@@ -1,11 +1,13 @@
-test_that("Workspace'deki değişkenleri ve dosyayı silme testi", {
-  
+test_that("Workspace'teki tüm değişkenleri silme", {
   rm(list = ls())
-  expect_equal(length(ls()), 0, info = "Workspace'deki değişkenlerin silindiğini kontrol etme")
-  
-  
-  file_exist <- file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")
-  expect_false(file_exist, info = "Dosyanın silindiğini kontrol etme")
+  expect_equal(length(ls()), 0)
+})
+
+test_that("Eğer mevcut ise veri setini silme", {
+  if ("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files()) {
+    file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+  }
+  expect_equal("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files(), FALSE)
 })
 
 current_dir <- getwd()
@@ -18,4 +20,9 @@ test_that("Dosya varlık testi", {
   # Dosyanın var olup olmadığını kontrol etme
   file_exist <- file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")
   expect_true(file_exist, info = "Dosyanın bulunduğunu kontrol etme")
+})
+
+#2.5
+test_that("Test : maps adlı değişken Global Workspace’de mevcuttur.", {
+  expect_true(exists("maps", envir = .GlobalEnv), info = "maps adlı değişken mevcut değil.")
 })
